73a6f94b311c32f50c839e3484444c95418bcfca
nxhieu3102 <60385614+nxhieu3102@users.noreply.github.com>
2021-08-23 23:39:12 +0700

    Add files via upload

---
+++ report.py
@@ -0,0 +1,44 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from datetime import date, datetime
+
+	
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = '/report_command/key.json'
+credentials = None
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# If modifying these scopes, delete the file token.json.
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1461y4vKxSoPwaDV6wUuLAqObNXwEWZ3Hmq27pKywbt0'
+ 
+service = build('sheets', 'v4', credentials=credentials)
+
+count = 0
+sheet = service.spreadsheets()
+
+# Call the Sheets API
+def send_report_infor(user_report, user_reported, data):
+        global count
+
+        if count == 0:
+                infor = [["Người báo cáo", "Người bị báo cáo","Thời gian"]]
+                request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range = "test!A1", valueInputOption = "USER_ENTERED",
+                                        body = {"values": infor}).execute()
+        count = count + 1
+        
+
+        now = datetime.now()
+        date_time = now.strftime("%d/%m/%Y %H:%M")
+        print(date_time)
+
+        infor = [user_report, user_reported, data]
+        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range = "test!A%s"%(count), valueInputOption = "USER_ENTERED",
+                                body = {"values": infor}).execute()
+        
+
