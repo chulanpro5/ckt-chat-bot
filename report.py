9b405aa25abb77c0e6af40d95b8d42c7f3ece375
Hồ Khánh Duy <68040131+chulanpro5@users.noreply.github.com>
2021-08-24 13:09:16 +0700

    add

---
+++ report.py
@@ -0,0 +1,43 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from datetime import date, datetime
+
+	
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'key.json'
+credentials = None
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# If modifying these scopes, delete the file token.json.
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1461y4vKxSoPwaDV6wUuLAqObNXwEWZ3Hmq27pKywbt0'
+ 
+service = build('sheets', 'v4', credentials=credentials)
+
+count = 0
+sheet = service.spreadsheets()
+
+# Call the Sheets API
+def send_report_infor(user_report, user_reported, data):
+        global count
+
+        if count == 0:
+                infor = [["Người báo cáo", "Người bị báo cáo","Thời gian"], [user_report, user_reported, data]]
+                request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range = "test!A1", valueInputOption = "USER_ENTERED",
+                                        body = {"values": infor}).execute()
+                count = 1
+                return
+                
+
+        count = count + 1
+        
+
+        infor = [user_report, user_reported, data]
+        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range = "test!A%s"%(count), valueInputOption = "USER_ENTERED",
+                                body = {"values": infor}).execute()
+        
