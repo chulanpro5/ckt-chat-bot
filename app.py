0586992b9ea6b34ae7fbaa076e490d89d7e726cc
Hồ Khánh Duy <68040131+chulanpro5@users.noreply.github.com>
2021-08-24 12:53:56 +0700

    test1

--- app.py
+++ app.py
@@ -1,44 +1,25 @@
 import requests
 from flask import Flask, request
+from flask import jsonify
 import json
 from datetime import date, datetime
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
-from flask import jsonify
-import pytz 
+from report import send_report_infor
 
 app = Flask(__name__)
 
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = '123456'  # <paste your verify token here>
-PAGE_ACCESS_TOKEN = 'EABAMQ9TnhaEBAEuiotgOmiopAZAeywIb8yL4vdgF2PE0Duhf9OM1tA6ZAsaJ3NfVS4pmGZCwPBQYJsJEVP3htqSkCZBalTDc1jy2IDZCZAXAZCyfMHHccSE6bAZAvzUZBpISGTEtWPbEbZBUYFCRgVpEXw3h86zxt2nBu81T0YGv0V8iVqRZAJ8wizb'  # paste your page access token here>"
+PAGE_ACCESS_TOKEN = 'EAAFZAXBWQCHQBAOksVHZBBrH2h4llnUR5ok9VZBdueMeE1E3ZArwQHGNVcxWe8Hguhq2IDFIkeXPQPpkuHnXmuAUSQmkrCucU1PeYA8OpLeq6YzVnteSnXmqyrrb69jABu1IZA8iL1hjc42kxZBU4dC14m5n683IakwCcRAmUDZCZC8e7jOdxvQF'  # paste your page access token here>"
 
+data = open('data_user.json',)
 
-data = open('data.json',)
 data = json.load(data)
-reply = open('reply.json',  encoding="utf8")
-reply = json.load(reply)
+
 user_data = data["user_data"]
 waiting_room = data["waiting_room"]
-count = data["count"]
 
-
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'key.json'
-credentials = None
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-SAMPLE_SPREADSHEET_ID = '1461y4vKxSoPwaDV6wUuLAqObNXwEWZ3Hmq27pKywbt0'
-service = build('sheets', 'v4', credentials=credentials)
-sheet = service.spreadsheets()
-
-
-def verify_webhook(req):
-    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
-        return req.args.get("hub.challenge")
-    else:
-        return "incorrect"
-
+reply = open('reply.json',encoding="utf8")
+reply = json.load(reply)
 
 def send_message(payload):
     auth = {
@@ -98,6 +79,10 @@ def send_attachment(recipient_id, attachment_url, type):
     send_message(payload)
 
 
+def is_seen(event):
+    return (event.get('read'))
+
+
 def is_command(event):
     if "postback" in event:
         if "payload" in event["postback"]:
@@ -106,16 +91,11 @@ def is_command(event):
         return 0
 
 
-def process_command(id, command):
-    if command == "started":
-        send_text(id, reply["started"])
-        create_user(id)
-    if command == "timban":
-        timban(id)
-    elif command == "ketthuc":
-        ketthuc(id)
-    elif command == "report":
-        report(id)
+def verify_webhook(req):
+    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
+        return req.args.get("hub.challenge")
+    else:
+        return "incorrect"
 
 
 def timban(id):
@@ -126,6 +106,24 @@ def timban(id):
     elif user_data[id]["state"] == "empty":
         find_partner(id)
 
+def ketthuc(id):
+    if user_data[id]["state"] == "connected":
+        disconnect(id)
+    elif user_data[id]["state"] == "waiting":
+        user_data[id]["state"] = "empty"
+        waiting_room["id"] = ""
+        waiting_room["state"] = "empty"
+        send_text(id, reply["ketthuc-waiting"])
+    elif user_data[id]["state"] == "empty":
+        send_text(id, reply["ketthuc-empty"])
+
+def report(id):
+    if user_data[id]["state"] == "connected":
+        partner_id = user_data[id]["partner_id"]
+        send_text(id, reply["report-connected"])
+        now = datetime.now()
+        date_time = now.strftime("%d/%m/%Y %H:%M")
+        send_report_infor(user_data[id]["user_name"], user_data[partner_id]["user_name"], date_time)
 
 def find_partner(id):
     if waiting_room["state"] == "empty":
@@ -150,18 +148,6 @@ def connect(id1, id2):
     send_text(id2, reply["timban-empty-empty"])
 
 
-def ketthuc(id):
-    if user_data[id]["state"] == "connected":
-        disconnect(id)
-    elif user_data[id]["state"] == "waiting":
-        user_data[id]["state"] = "empty"
-        waiting_room["id"] = ""
-        waiting_room["state"] = "empty"
-        send_text(id, reply["ketthuc-waiting"])
-    elif user_data[id]["state"] == "empty":
-        send_text(id, reply["ketthuc-empty"])
-
-
 def disconnect(id):
     partner_id = user_data[id]["partner"]
 
@@ -174,39 +160,60 @@ def disconnect(id):
     send_text(partner_id, reply["ketthuc-connected"])
 
 
-def report(id):
+def send_to_partner(id, message_data, message_type):
     if user_data[id]["state"] == "connected":
-        partner_id = user_data[id]["partner"]
-        timezone = pytz.timezone('Asia/Saigon')
-        now = datetime.now(timezone)
-        date_time = now.strftime("%d/%m/%Y %H:%M")
-        send_report_infor(user_data[id]["user_name"],
-                          user_data[partner_id]["user_name"], date_time, id)
+        if message_type == "text":
+            send_text(user_data[id]["partner"], message_data)
+        else: 
+            send_attachment(user_data[id]["partner"], message_data, message_type)
+    else:
+        send_text(id, reply["send_to_partner-notconnected"])
 
 
-def send_report_infor(user_report, user_reported, data, id):
-    global count
+def create_user_data(id):
+    if id not in user_data:
+        profile_URL = "https://graph.facebook.com/%s?fields=name&access_token=%s"%(id, PAGE_ACCESS_TOKEN)
+        response = requests.get(profile_URL)
 
-    if count == 1:
-        infor = [["Người báo cáo", "Người bị báo cáo", "Thời gian"]]
-        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                        range="test!A1", valueInputOption="USER_ENTERED",
-                                        body={"values": infor}).execute()
-    count = count + 1
+        user_data[id] = {}
+        print(response.json())
+        user_data[id]["user_name"] = response.json()["name"]
+        user_data[id]["state"] = "empty"
+        user_data[id]["partner"] = "empty"
 
-    pos = "test!A" + str(count)
-    infor = [[user_report, user_reported, data]]
 
-    print("----------------------------")
-    print(infor)
-    print(count)
-    print(pos)
-    print("----------------------------")
-    send_text(id, reply["report-connected"])
+def load_data():
+    global user_data
+    global waiting_room
 
-    request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=pos, valueInputOption="USER_ENTERED",
-                                    body={"values": infor}).execute()
+    data = open('data_user.json',)
+
+    data = json.load(data)
+
+    user_data = data["user_data"]
+    waiting_room = data["waiting_room"]
+
+
+def save_data():
+    global data
+
+    data["waiting_room"] = waiting_room
+    data["user_data"] = user_data
+
+    with open('data_user.json', 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+
+def process_command(id, command):
+    if command == "started":
+        create_user_data(id)
+        send_text(id, reply["started"])
+    if command == "timban":
+        timban(id)
+    elif command == "ketthuc":
+        ketthuc(id)
+    elif command == "report":
+        report(id)
 
 
 def is_user_message(event):
@@ -230,56 +237,7 @@ def get_message(event):
 
     return message_type, message_data
 
-
-def send_to_partner(id, message_data, message_type):
-    if user_data[id]["state"] == "connected":
-        if message_type == "text":
-            send_text(user_data[id]["partner"], message_data)
-        else:
-            send_attachment(user_data[id]["partner"],
-                            message_data, message_type)
-    else:
-        send_text(id, reply["send_to_partner-notconnected"])
-
-
-def is_seen(event):
-    return (event.get('read'))
-
-
-def create_user(id):
-    if id not in user_data:
-        profile_URL = "https://graph.facebook.com/%s?fields=name&access_token=%s" % (
-            id, PAGE_ACCESS_TOKEN)
-        response = requests.get(profile_URL)
-
-        user_data[id] = {}
-        user_data[id]["user_name"] = response.json()["name"]
-        user_data[id]["state"] = "empty"
-        user_data[id]["partner"] = "empty"
-
-
-def load_data():
-    global user_data
-    global waiting_room
-    global count
-    data = open('data.json',)
-
-    data = json.load(data)
-
-    user_data = data["user_data"]
-    waiting_room = data["waiting_room"]
-    count = data["count"]
-
-
-def save_data():
-    global data
-
-    data["waiting_room"] = waiting_room
-    data["user_data"] = user_data
-    data["count"] = count
-    with open('data.json', 'w') as fp:
-        json.dump(data, fp, indent=4)
-
+    
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def listen():
@@ -290,29 +248,38 @@ def listen():
 
     if request.method == 'POST':
         load_data()
+
         payload = request.json
         event = payload['entry'][0]['messaging']
+        #print(event)
         for current_event in event:
             print(current_event)
 
             if is_command(current_event):
                 sender_id = current_event['sender']['id']
+                #create_user_data(sender_id)
                 send_action(sender_id, "typing_on")
+
                 command = current_event["postback"]["payload"]
                 process_command(sender_id, command)
-
+            
             elif is_user_message(current_event):
                 sender_id = current_event['sender']['id']
+                #create_user_data(sender_id)
+
                 message_type, message_data = get_message(current_event)
                 send_to_partner(sender_id, message_data, message_type)
 
             if is_seen(current_event):
                 sender_id = current_event['sender']['id']
-                if sender_id in user_data and user_data[sender_id]["state"] == "connected":
+                #create_user_data(sender_id)
+
+                if user_data[sender_id]["state"] == "connected":
                     send_action(user_data[sender_id]["partner"], "mark_seen")
 
+            # save_data()
+            
         save_data()
-
         return jsonify(result={"status": 200})
 
 
